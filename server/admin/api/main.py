12dfb3c7794bd712548019a22bc73e9854c5ca87
mirzafar <m_mirzafar@mail.ru>
2024-02-19 01:11:57 +0600

    s

--- server/admin/api/main.py
+++ server/admin/api/main.py
@@ -32,3 +32,21 @@ class OrdersView(HTTPMethodView):
             '_success': True,
             'order': dict(order)
         })
+
+    async def post(self, request):
+        action = request.json and request.json.get('action')
+        order = request.json and request.json.get('order')
+        if action == 'done' and order:
+            await db.fetchrow(
+                '''
+                UPDATE public.orders
+                SET status = 2, path = $2
+                WHERE id = $1
+                ''',
+                order['id'],
+                order['path'],
+            )
+
+        return response.json({
+            '_success': True
+        })
